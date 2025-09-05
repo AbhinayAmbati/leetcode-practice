a3670a041e04e40f810ad94996f67e9e59d1de37
AbhinayAmbati <abhinayambati4@gmail.com>
2025-09-05 20:03:19 +0530

    Solved Make the Integer Zero problem time : 20:03 data : 05-09-2025

---
+++ MakeTheIntegerZero.java
@@ -0,0 +1,11 @@
+public class MakeTheIntegerZero(int num1 , int num2){
+    for(int k=1;k<=60;k++){
+        long target = (long)num1 - (long)(k*num2);
+        if(target < 0) continue;
+        int bitCount = Long.BitCount(target);
+        if(bitCount <= k && k <= target){
+            return k;
+        }
+    }
+    return -1;
+}
